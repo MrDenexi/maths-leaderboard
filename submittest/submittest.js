cf08cf2333c96797d555c0095ba4aefa93baa1a9
MrDenexi <25987844+MrDenexi@users.noreply.github.com>
2018-08-08 13:00:08 +0200

    submittest extended

--- submittest/submittest.js
+++ submittest/submittest.js
@@ -1,5 +1,7 @@
-let token = 'g58vsjR7sPDROm05Z6UqfPplNFe6txDtq377B0U0R8I='
-let data = [{_id: 'Comakip', answers: 38},{_id: 'ruff', answers: 3},{_id: 'Jerry', answers: 11},{_id: 'Ballk', answers: 12},]
+let token = 'yB7J9NP1dWHUGUdZwvck5YEKMTeAhJaboNzkeJZiSCQ=';
+let data = [{_id: 'Comakip', answers: 38},{_id: 'ruff', answers: 3},{_id: 'Jerry', answers: 11},{_id: 'Ballk', answers: 12}];
+let data1 = {_id: 'Comakip', answers: 38};
+console.log(JSON.stringify(data1));
 
 function json(response) {
   return response.json()
@@ -8,11 +10,10 @@ function json(response) {
 window.fetch('http://localhost:8002/submit', {
     method: 'POST',
     headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Content-type": "application/json",
         "Authorization": `bearer ${token}`
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(data)
 })
-.then(json)
-.then( (data) => console.log('Request succeeded with JSON response', data))
-.catch( (err) => console.log('Request failed', err))
+.then(response => console.log(response)) // parses response to JSON
+.catch(error => console.error(`Fetch Error =\n`, error));
